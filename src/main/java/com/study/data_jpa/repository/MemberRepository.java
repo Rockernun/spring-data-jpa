0c87b23759fe128e3fba01b6237c2f84a64c33e0
rockernun <byunguk98@naver.com>
2026-09-22 16:41:59 +0900

    feat: @Query로 단순 값 및 DTO 조회 메서드 추가
    - findUsernameList: username만 String 리스트로 조회
    - findMemberDto: JPQL new 연산자로 Member-Team 조인 후 MemberDto로 반환

--- src/main/java/com/study/data_jpa/repository/MemberRepository.java
+++ src/main/java/com/study/data_jpa/repository/MemberRepository.java
@@ -1,5 +1,6 @@
 package com.study.data_jpa.repository;
 
+import com.study.data_jpa.dto.MemberDto;
 import com.study.data_jpa.entity.Member;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -12,4 +13,10 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
 
     @Query("select m from Member m where m.username = :username and m.age = :age")
     List<Member> findUser(@Param("username") String username, @Param("age") int age);
+
+    @Query("select m.username from Member m ")
+    List<String> findUsernameList();
+
+    @Query("select new com.study.data_jpa.dto.MemberDto(m.id, m.username, t.name) from Member m join m.team t")
+    List<MemberDto> findMemberDto();
 }
